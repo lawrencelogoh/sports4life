1b9d57e0cf6fce7f5cedcee75b46455dec4e888b
SabiMantock <sabimantock@gmail.com>
2024-03-24 11:47:26 +0000

    feat: focus areas page

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,4 +15,41 @@ document.addEventListener('DOMContentLoaded', function () {
   mobileNavbarOpenBtn.addEventListener('click', function () {
     mobileMenu.style.top = '-100%';
   });
+  // Get all carousel slides
+  const slides = document.querySelectorAll('.carousel-slide');
+  let currentSlide = 0;
+
+  // Function to show slide
+  function showSlide() {
+    // Hide all slides
+    slides.forEach((slide) => {
+      slide.style.display = 'none';
+    });
+
+    // Show current slide
+    slides[currentSlide].style.display = 'block';
+
+    // Increment current slide index
+    currentSlide++;
+
+    // Reset current slide index if it exceeds the total number of slides
+    if (currentSlide >= slides.length) {
+      currentSlide = 0;
+    }
+  }
+
+  // Show the first slide initially
+  showSlide();
+
+  // Automatically advance slides every 5 seconds (adjust interval as needed)
+  setInterval(() => {
+    showSlide();
+  }, 5000);
 });
+
+// const swiper = new Swiper('.swiper', {
+//   // Optional parameters
+//   direction: 'horizontal',
+//   loop: true,
+//   autoplay: false,
+// });
